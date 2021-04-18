67dd641349612ab3cbad01dac0746978fc4df6b2
Christina Sickelco <csickelco@gmail.com>
2021-04-17 21:19:40 -0400

    Local time conversions

--- get_daylight_times.py
+++ get_daylight_times.py
@@ -1,15 +1,22 @@
 # importing the requests library
 import requests
+from datetime import datetime
+from dateutil import tz
+
+to_zone = tz.gettz('America/New_York')
+
+#Constants
+ISO_8601_FORMAT = "%Y-%m-%dT%H:%M:%S%z"
 
 # api-endpoint
 URL = "https://api.sunrise-sunset.org/json"
 
 # parameters
 latitude = "43.161030"
-longitude="-77.610924"
-dateVal="tomorrow"
+longitude = "-77.610924"
+dateVal = "tomorrow"
 
-PARAMS = {'lat':latitude, 'lng':longitude, 'date':dateVal}
+PARAMS = {'lat':latitude, 'lng':longitude, 'date':dateVal, 'formatted': 0}
 
 # sending get request and saving the response as response object
 r = requests.get(url=URL, params=PARAMS)
@@ -18,8 +25,10 @@ r = requests.get(url=URL, params=PARAMS)
 data = r.json()
 
 # extracting sunrise, civil_twilight_begin
-sunrise = data['results']['sunrise']
-civilTwilightBegin = data['results']['civil_twilight_begin']
+sunriseUTC = datetime.strptime(data['results']['sunrise'], ISO_8601_FORMAT)
+sunriseLocal = sunriseUTC.astimezone(to_zone)
+civilTwilightBeginUTC = datetime.strptime(data['results']['civil_twilight_begin'], ISO_8601_FORMAT)
+civilTwilightBeginLocal = civilTwilightBeginUTC.astimezone(to_zone)
 
 # printing the output
-print(f'sunrise: {sunrise}, twilight_begin: {civilTwilightBegin}')
+print(f'sunrise: {sunriseLocal.time()}\ntwilight_begin: {civilTwilightBeginLocal.time()}')
